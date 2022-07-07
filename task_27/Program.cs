6cfc508f73afc874d79ee8b45d699b32eab652b0
Spec-75 <Spec_75@mail.ru>
2022-07-07 18:57:22 +0300

    add .origin

---
+++ task_27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму 
+//цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+//int sum = 0;
+string[] arr = number;
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+PrintArray(number);
+// for (int i = 1; i<=number; i++)
+// {
+//     sum += i;
+// }
+// Console.WriteLine($"Cумма чисел: {sum}");
